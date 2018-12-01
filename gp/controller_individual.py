a9d6979176c77f499e1e599a7a14059a1c2cbcdb
William Lorey <wwlorey@gmail.com>
2018-12-01 16:50:49 -0600

    Add bonus comments to source

--- gp/controller_individual.py
+++ gp/controller_individual.py
@@ -1,5 +1,7 @@
 class ControllerIndividual:
     def __init__(self, conts, fitness=0):
         """Initializes the ControllerIndividual class."""
-        self.conts = conts
         self.fitness = fitness
+
+        # BONUS1, BONUS2*
+        self.conts = conts
